58da456875c2c67e0e192d0d4dae1cd880225f73
Thiago Lioy <lioy@Thiagos-MacBook-Air-2.local>
2015-05-22 02:10:43 -0300

    updated tests , still need to do it right

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,38 +8,14 @@
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
 
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
+describe(@"need tests", ^{
     
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
-        
-        });
-    });
-});
-
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
+    it(@"will pass , please do something better", ^{
         expect(1).beLessThan(23);
     });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^{
-        waitUntil(^(DoneCallback done) {
-            done();
-        });
-    });
+
 });
 
 SpecEnd
